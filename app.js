9fe155e474f4578bc183eacb414cc47d0b753420
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-06-12 13:39:57 +0530

    connected api thorugh js

---
+++ app.js
@@ -0,0 +1,18 @@
+var btnTranslate=document.querySelector(".btn");
+
+var txtInput=document.querySelector(".text-area");
+
+var outputDiv=document.querySelector(".output-div");
+
+var serverURL="https://api.funtranslations.com/translate/shakespeare.json";
+function getserverURL(text)
+{
+    return serverURL+"?"+"text="+text;
+}
+
+function clickEventHandler(){
+
+    fetch(getserverURL(txtInput.value)).then(response => response.json()).then(json => outputDiv.innerText=json.contents.translated);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
